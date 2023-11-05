39e814ef9413456554278b57b78fd07aa10ddd63
Rosa Rivera <rosa.riverasoto43@myhunter.cuny.edu>
2023-11-05 16:19:32 -0500

    lalala

--- project 1/cpp/ActionCard.cpp
+++ project 1/cpp/ActionCard.cpp
@@ -1,1 +1,30 @@
+#include "actioncard.hpp"
+
+// ActionCard Constructor
+ActionCard::ActionCard() {
+    setType(CardType::ACTION_CARD);
+}
+bool ActionCard::isPlayable() {
+    return getDrawn();
+}
+void ActionCard::Print() const {
+    std::cout << "Type: " << getType() << std::endl;
+    std::cout << "Instruction: " << getInstruction() << std::endl;
+    std::cout << "Card:" << std::endl;
+
+    if (getDrawn()) {
+        const int* imageData = getImageData();
+        if (imageData) {
+            for (int i = 0; imageData[i] != 0; i++) {
+                std::cout << imageData[i] << " ";
+            }
+        } else {
+            std::cout << "No image data";
+        }
+    } else {
+        std::cout << "Card not drawn yet";
+    }
+
+    std::cout << std::endl;
+}
 
--- project 1/cpp/PointCard.cpp
+++ project 1/cpp/PointCard.cpp
@@ -1,1 +1,32 @@
+#include "pointcard.hpp"
+#include "card.hpp"
+
+// PointCard Constructor
+PointCard::PointCard() {
+    setType(CardType::POINT_CARD); 
+}
+bool PointCard::isPlayable() {
+    return getDrawn();
+}
+void PointCard::Print() const {
+    std::cout << "Type: " << getType() << std::endl;
+    std::cout << "Points: " << getInstruction() << std::endl;
+    std::cout << "Card:" << std::endl;
+
+    if (getDrawn()) {
+        
+        const int* imageData = getImageData();
+        if (imageData) {
+            for (int i = 0; imageData[i] != 0; i++) {
+                std::cout << imageData[i] << " ";
+            }
+        } else {
+            std::cout << "No image data";
+        }
+    } else {
+        std::cout << "Card not drawn yet";
+    }
+
+    std::cout << std::endl;
+}
 
